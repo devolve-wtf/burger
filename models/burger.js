a667cfb1de815c7ad4d639a17532e87cbd6ee944
kenput3r <devolve.wtf@gmail.com>
2017-09-23 20:59:43 -0700

    complete Model Setup

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require('../config/orm.js');
+
+const burger = {
+    selectAll: function(callback) {
+        orm.selectAll('burgers', function(res) {
+            callback(res);
+        });
+    },
+    insertOne: function(cols, vals, callback) {
+        orm.insertOne('burgers', cols, vals, function(res) {
+            callback(res);
+        });
+    },
+    updateOne: function(objColVals, condition, callback) {
+        orm.updateOne('burgers', objColVals, condition, function(res) {
+            callback(res);
+        });
+    }
+}
+
+module.exports = burger;
