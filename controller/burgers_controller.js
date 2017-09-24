d9a7ba1a3bb8fdeaa340e32ade40b86f87ad7fd1
kenput3r <devolve.wtf@gmail.com>
2017-09-23 21:14:25 -0700

    Complete Controller Setup

---
+++ controller/burgers_controller.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const burger = require('../models/burger.js');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+        let object = {
+            burgers: data
+        };
+        //Log for testing
+        console.log(object);
+        res.render('index', object);
+    });
+});
+
+router.post('/', function(req, res) {
+    burger.insertOne([
+        'burger_name', 'devoured'
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function() {
+        res.redirect('/');
+    });
+});
+
+router.put('/:id', function(req, res) {
+    let condition = 'id = ' + req.params.id;
+
+    //Log for testing
+    console.log('condition', condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function() {
+        res.redirect('/');
+    });
+})
