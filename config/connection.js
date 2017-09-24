e0d6ca715dfb1e1a0bc0c16f72c40d4d311c971b
kenput3r <devolve.wtf@gmail.com>
2017-09-23 22:41:23 -0700

    conditional for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: 'root',
-    password: 'root',
-    database: 'burger_db',
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
-});
+let connection;
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: 'root',
+        password: 'root',
+        database: 'burger_db',
+        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+    });
+}
 
 connection.connect(function(err) {
     if(err) {
